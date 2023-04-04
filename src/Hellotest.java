d7c283d02ff4dce8fc7bee2d37b586307154526f
zxasqwsz <zxasqwsz@naver.com>
2023-04-04 16:49:09 +0900

    Update Hellotest.java

--- src/Hellotest.java
+++ src/Hellotest.java
@@ -3,10 +3,5 @@ public class Hellotest {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello world2");
-
-		System.out.println("Hello world3");
-
-		System.out.println("Hello world3");
-		System.out.println("Hello world3");
 	}
 }
